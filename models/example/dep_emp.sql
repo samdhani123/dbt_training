0ea2f406560b26c03bd7438db3ff4088bd4ec90b
samdhani mulla <hariss19988@gmail.com>
2026-08-16 19:17:39 +0000

    ss

--- models/example/dep_emp.sql
+++
@@ -1,7 +0,0 @@
-{{config(materialized='table')}}
-
-select emp_id,first_name,last_name,
-first_name||last_name as full_name,
-sal as emp_salary
-from 
-{{ref('emp_d_p_tables')}}
--- models/example/emp_t.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='table') }}
-
-select * from DBT_SOURCE.D_SOURCE.emp
